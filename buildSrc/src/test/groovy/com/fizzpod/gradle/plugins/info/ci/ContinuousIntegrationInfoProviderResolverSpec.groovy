d56d9726a01aa70a0bd607e91c35f35ad1ba4ae8
Andrew Dunn <andy.dunn@expensys.com>
2015-02-27 23:02:06 +0000

    Added wercker provider

--- buildSrc/src/test/groovy/com/fizzpod/gradle/plugins/info/ci/ContinuousIntegrationInfoProviderResolverSpec.groovy
+++ buildSrc/src/test/groovy/com/fizzpod/gradle/plugins/info/ci/ContinuousIntegrationInfoProviderResolverSpec.groovy
@@ -16,11 +16,12 @@ class ContinuousIntegrationInfoProviderResolverSpec extends ProjectSpec {
 		then:
 		def providers = resolver.all()
 		providers != null
-		providers.size() == 4
+		providers.size() == 5
 		providers[0].getClass().equals(JenkinsProvider.class)
 		providers[1].getClass().equals(DroneIoProvider.class)
 		providers[2].getClass().equals(ShippableProvider.class)
-		providers[3].getClass().equals(UnknownContinuousIntegrationProvider.class)
+		providers[3].getClass().equals(WerckerProvider.class)
+		providers[4].getClass().equals(UnknownContinuousIntegrationProvider.class)
 	}
 
 	def 'get Jenkins provider if running on Jenkins'() {
@@ -28,6 +29,7 @@ class ContinuousIntegrationInfoProviderResolverSpec extends ProjectSpec {
 		def onJenkins = System.getenv('BUILD_NUMBER') && System.getenv('JOB_NAME')
 		def onDrone = System.getenv('DRONE')
 		def onShippable = System.getenv('SHIPPABLE')
+		def onWercker = System.getenv('WERCKER_ROOT')
 		def resolver = new ContinuousIntegrationInfoProviderResolver()
 
 		then:
@@ -38,6 +40,8 @@ class ContinuousIntegrationInfoProviderResolverSpec extends ProjectSpec {
 			provider.getClass().equals(DroneIoProvider.class)
 		} else if(onShippable) {
 			provider.getClass().equals(ShippableProvider.class)
+		} else if(onWercker) {
+			provider.getClass().equals(WerckerProvider.class)
 		} else {
 			provider.getClass().equals(UnknownContinuousIntegrationProvider.class)
 		}
